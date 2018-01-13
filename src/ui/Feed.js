8eb2aa30760fa1fd8e56ec4acb545675e310cd29
Craig Ambrose <craig@craigambrose.com>
2018-01-13 11:40:44 +1100

    Displays the ten most recent message keys

--- src/ui/Feed.js
+++ src/ui/Feed.js
@@ -1,17 +1,47 @@
 import React, {Component} from 'react'
 
-class Feed extends Component {
-  render () {
-    return (
-      <box top='center'
-        left='center'
-        width='50%'
-        height='50%'
-        border={{type: 'line'}}
-        style={{border: {fg: 'blue'}}}>
-        The Feed
-      </box>
+const Client = require('ssb-client')
+const pull = require('pull-stream')
+
+function loadMessages (handleMessage) {
+  Client((err, sbot) => {
+    if (err) throw err
+
+    var source = sbot.createFeedStream({ reverse: true, limit: 10 })
+    var sink = pull.drain(handleMessage)
+
+    pull(
+      source,
+      sink
     )
+  })
+}
+
+class Feed extends Component {
+  constructor () {
+    super()
+    this.state = {messages: []}
+  }
+
+  componentDidMount () {
+    loadMessages(this.receivedMessage.bind(this))
+  }
+
+  receivedMessage (message) {
+    this.setState({messages: [message, ...this.state.messages]})
+  }
+
+  render () {
+    const messages = this.state.messages || []
+    return (
+      <layout width='95%' height='95%'>
+        { messages.map(this.renderMessage.bind(this)) }
+      </layout>
+    )
+  }
+
+  renderMessage (message) {
+    return <box key={message.key} width='90%'><text>{message.key}</text></box>
   }
 }
 
--- src/ui/Layout.js
+++ src/ui/Layout.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react'
 import Feed from './Feed'
 
+const stylesheet = {
+  bordered: {
+    border: {
+      type: 'line'
+    },
+    style: {
+      border: {
+        fg: 'blue'
+      }
+    }
+  }
+}
+
 class Layout extends Component {
   render () {
     return (
-      <box top='center'
-        left='center'
-        width='50%'
-        height='50%'
-        border={{type: 'line'}}
-        style={{border: {fg: 'blue'}}}>
+      <box top='center' left='center' width='100%' height='100%' label=' Your Scuttlebutt Feed ' class={stylesheet.bordered}>
         <Feed />
       </box>
     )
