5516c771bb8d16088e318a0e1a92a8f2d112b71d
Craig Ambrose <craig@craigambrose.com>
2018-01-13 15:13:36 +1100

    More tests

--- src/feeds/ThreadWindow.js
+++ src/feeds/ThreadWindow.js
@@ -1,15 +1,41 @@
-import { map } from 'lodash'
+import { map, merge, find } from 'lodash'
+
+const defaultOptions = {
+  maxSize: 10
+}
 
 export default class ThreadWindow {
-  constructor () {
+  constructor (options = {}) {
     this.roots = []
+    this.lobby = []
+    this.options = merge(defaultOptions, options)
   }
 
   addRoot (msg) {
-    this.roots.push(msg)
+    if (this._isFull()) {
+      this._addToLobby(msg)
+    } else {
+      if (!find(this.roots, { key: msg.key })) {
+        this.roots.push(msg)
+      }
+    }
   }
 
   rootKeys () {
     return map(this.roots, 'key')
+  }
+
+  lobbyKeys () {
+    return map(this.lobby, 'key')
+  }
+
+  // PRIVATE
+
+  _isFull () {
+    return this.roots.length >= this.options.maxSize
+  }
+
+  _addToLobby (msg) {
+    this.lobby.push(msg)
   }
 }
--- test/feeds/ThreadWindow.test.js
+++ test/feeds/ThreadWindow.test.js
@@ -1,18 +1,45 @@
 import ThreadWindow from '../../src/feeds/ThreadWindow'
 import expect from 'expect.js'
 
+function buildRootPost (key) {
+  return {
+    key: key,
+    value: { type: 'post', text: 'Introducing myself' }
+  }
+}
+
 describe('ThreadWindow', function () {
   describe('adding roots', function () {
-    it('adds the first root', function () {
-      const rootMessage = {
-        key: 'intro',
-        value: { type: 'post', text: 'Introducing myself' }
-      }
-      const window = new ThreadWindow()
+    context('when the window isnt full', function () {
+      it('adds the first root', function () {
+        const window = new ThreadWindow()
+        window.addRoot(buildRootPost('intro'))
+        expect(window.rootKeys()).to.eql(['intro'])
+      })
 
-      window.addRoot(rootMessage)
+      it('doesnt add the same root twice', function () {
+        const window = new ThreadWindow()
+        window.addRoot(buildRootPost('intro'))
+        window.addRoot(buildRootPost('intro'))
+        expect(window.rootKeys()).to.eql(['intro'])
+      })
+    })
 
-      expect(window.rootKeys()).to.eql(['intro'])
+    describe('when the window is full', function () {
+      it('holds newer message in the lobby', function () {
+        const window = new ThreadWindow({maxSize: 1})
+        window.addRoot(buildRootPost('intro'))
+        window.addRoot(buildRootPost('second'))
+        expect(window.rootKeys()).to.eql(['intro'])
+        expect(window.lobbyKeys()).to.eql(['second'])
+      })
     })
   })
 })
+
+// A new root message creates a thread and adds it to the window
+// On a new reply
+//    if it already has a thread in the window
+//        wants to update that thread
+//    if it doesn't have a thread in the window
+//        wants to create a thread and offer it to the window
